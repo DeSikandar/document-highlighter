242f8ff0056f225ba7056cf219f1c4438c0e556b
sikandar patel <pateltu.sp@gmail.com>
2024-01-24 00:49:18 +0530

    Document Highlighter

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # document-highlighter
+
+Use NLP for the text Highlighting with spaCy
+SpaCy have pre-train model for the NLP so using this we will get the Named Entity Recognition (NER) and using NER we will mark the word or sentence highlighted
+
+With using NLP approach we can achieve the High accuracy but need to download pre-train model and may be computationally expensive.
+
+For the document viewer i have use the simple html for now in the html we can easily highlight the selected text
+
+i have also try with pdfjs library but there are some module is outdated  for that library for the highlight the text like TextLayerBuilder to extract the full text so we can use the pdfjs for the view purpose only there are some good library Adobe PDF Embed API, PDF.js Express but this are paid library but it will support the highlighting we can pass the NER in that library as array and get highlighted text
+
+
+i have also include the TestPA.pdf file that i am using for the testing 
+
+
+# install pipenv 
+pip install pipenv
+
+# install dependency
+pipenv shell 
+pipenv install 
+python -m spacy download en_core_web_sm
+
+# run the app 
+python src/main.py
+
+# step for process the file
+
+once app is started navigate to http://127.0.0.1:5000
+and upload pdf file there and hit Submit btn and you get the highlighted view of the document 
---
+++ src/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import spacy
+import PyPDF2
+import re
+import base64
+
+
+app = Flask(__name__)
+
+nlp = spacy.load("en_core_web_sm")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/highlight',methods=['POST'])
+def get_heigh_lighted():
+    pdf_file = request.files['pdfFile']
+    document_text = extract_text_from_pdf(pdf_file)
+    doc = nlp(document_text)
+    extracted_entities = [ent.text for ent in doc.ents]
+    # Highlight extracted entities
+    highlighted_document = highlight_text(document_text, extracted_entities)
+    return render_template('result.html', highlighted_document=highlighted_document)
+    
+    
+def extract_text_from_pdf(pdf_file):
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    text = ""
+
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+
+    return text
+
+def highlight_text(document, entities):
+    highlighted_document = document
+    for entity in entities:
+        highlighted_document = re.sub(r'\b' + re.escape(entity) + r'\b', 
+                                      f'<span class="highlight">{entity}</span>', 
+                                      highlighted_document, flags=re.IGNORECASE)
+    return highlighted_document
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
